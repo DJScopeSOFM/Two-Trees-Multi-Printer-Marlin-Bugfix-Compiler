87339f99a85a8760dd75ca0879b70a516901ab6c
DJScopeSOFM <73513667+DJScopeSOFM@users.noreply.github.com>
2020-11-09 14:54:26 +1100

    update

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -990,14 +990,14 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -15, 30, 0 }
+#define NOZZLE_TO_PROBE_OFFSET { -15, 30, -2.1 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
 #define PROBING_MARGIN 20
 
 // X and Y axis travel speed (mm/min) between probes
-#define XY_PROBE_SPEED (133*60)
+#define XY_PROBE_SPEED (150*60)
 
 // Feedrate (mm/min) for the first approach when double-probing (MULTIPLE_PROBING == 2)
 #define Z_PROBE_SPEED_FAST HOMING_FEEDRATE_Z
