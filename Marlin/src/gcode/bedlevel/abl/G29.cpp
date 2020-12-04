ee2f3d8892c0029e96626ee226e86c35f5501029
DJScopeSOFM <73513667+DJScopeSOFM@users.noreply.github.com>
2020-12-04 16:14:37 +1100

    Revert "Merge Latest Bugfix community fixes"
    This reverts commit 35dc04abaee4017a8fd1a0826865d645392e7aa3.

--- Marlin/src/gcode/bedlevel/abl/G29.cpp
+++ Marlin/src/gcode/bedlevel/abl/G29.cpp
@@ -656,9 +656,8 @@ G29_TYPE GcodeSuite::G29() {
 
           #elif ENABLED(AUTO_BED_LEVELING_BILINEAR)
 
-            const float z = measured_z + zoffset;
-            z_values[meshCount.x][meshCount.y] = z;
-            TERN_(EXTENSIBLE_UI, ExtUI::onMeshUpdate(meshCount, z));
+            z_values[meshCount.x][meshCount.y] = measured_z + zoffset;
+            TERN_(EXTENSIBLE_UI, ExtUI::onMeshUpdate(meshCount, z_values[meshCount.x][meshCount.y]));
 
           #endif
 
